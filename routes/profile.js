7a3656e921b63c8757f52aa4dc43d67d7b7af3a5
sharma39vishal <sharma39vishal@gmail.com>
2024-02-22 22:20:28 +0530

    Connect EditProfile Api

--- routes/profile.js
+++ routes/profile.js
@@ -12,4 +12,23 @@ router.get('/userdata', authMiddleware, async (req, res) => {
   }
 });
 
+router.post('/update', authMiddleware, async (req, res) => {
+  try {
+    if (!req.body.username || !req.body.email) {
+      return res.status(400).json({ message: 'Username and email are required' });
+    }
+    if (req.body.username.length < 3) {
+      return res.status(400).json({ message: 'Username must be at least 3 characters long' });
+    }
+    // use regex to check mail
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)) {
+      return res.status(400).json({ message: 'Invalid email address' });
+    }
+    const user = await User.findByIdAndUpdate(req.userId,{username:req.body.username,email:req.body.email});
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
